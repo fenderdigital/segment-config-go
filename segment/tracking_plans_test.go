d5e5ba8319db57b8977b1f598b15a286963b10b5
rae89 <raespinoza109@gmail.com>
2019-09-06 17:05:14 -0700

    add test for get tracking plan

--- segment/tracking_plans_test.go
+++ segment/tracking_plans_test.go
@@ -13,17 +13,17 @@ func TestTrackingPlans_ListTrackingPlans(t *testing.T) {
 	setup()
 	defer teardown()
 
-	endpoint := fmt.Sprintf("/%s/%s/%s/%s", apiVersion, WorkspacesEndpoint, testWorkspace, TrackingPlanEndpoint)
+	endpoint := fmt.Sprintf("/%s/%s/%s/%s/", apiVersion, WorkspacesEndpoint, testWorkspace, TrackingPlanEndpoint)
 
 	mux.HandleFunc(endpoint, func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, `{
 			"tracking_plans": [
 				{
-					"name": "workspace/myworkspace/tracking-plans/rs_123",
+					"name": "workspaces/myworkspace/tracking-plans/rs_123",
 					"display_name": "Kicks App",
 					"rules": {
-						"identify": [],
-						"group": [],
+						"identify_traits": [],
+						"group_traits": [],
 						"events": []
 					},
 					"create_time": "2019-02-05T00:28:31Z",
@@ -37,23 +37,258 @@ func TestTrackingPlans_ListTrackingPlans(t *testing.T) {
 	fmt.Printf("ERROR %+v\n", err)
 	assert.NoError(t, err)
 
-	currentTime, _ := time.Parse(time.RFC3339, "2019-02-05T00:28:31Z")
+	createTime, _ := time.Parse(time.RFC3339, "2019-02-05T00:28:31Z")
 	updatedTime, _ := time.Parse(time.RFC3339, "2019-02-05T00:28:31Z")
-	time.Parse(time.RFC3339, "2019-02-05T00:28:31Z")
 	expected := TrackingPlans{TrackingPlans: []TrackingPlan{
 		{
-			Name:        "workspace/myworkspace/tracking-plans/rs_123",
+			Name:        "workspaces/myworkspace/tracking-plans/rs_123",
 			DisplayName: "Kicks App",
 			Rules: Rules{
-				Identify: []Rule{},
-				Group:    []Rule{},
-				Events:   []Event{},
+				IdentifyTraits: []Rule{},
+				GroupTraits:    []Rule{},
+				Events:         []Event{},
 			},
-			CreateTime: currentTime,
+			CreateTime: createTime,
 			UpdateTime: updatedTime,
 		},
 	}}
-	fmt.Printf("ACTUAL %+v\n", actual)
-	fmt.Printf("EXPECTED %+v\n", expected)
+	fmt.Printf("ACTUAL:: %+v\n", actual)
+	assert.Equal(t, expected, actual)
+}
+
+func TestTrackingPlans_GetTrackingPlan(t *testing.T) {
+	setup()
+	defer teardown()
+
+	testTrackingPlan := "rs_123"
+	endpoint := fmt.Sprintf("/%s/%s/%s/%s/%s/",
+		apiVersion, WorkspacesEndpoint, testWorkspace, TrackingPlanEndpoint, testTrackingPlan)
+
+	mux.HandleFunc(endpoint, func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w,
+			`{
+				"name": "workspaces/myworkspace/tracking-plans/rs_123",
+				"display_name": "Kicks App",
+				"rules": {
+					"identify_traits": [],
+			  		"group_traits": [],
+					"events": [
+						{
+							"name": "Order Completed",
+							"description": "Who bought what",
+							"version": 1,
+							"rules": {
+							  "$schema": "http://json-schema.org/draft-07/schema#",
+							  "type": "object",
+							  "properties": {
+								"context": {},
+								"traits": {},
+								"properties": {
+								  "required": [
+									"product",
+									"price",
+									"amount"
+								  ],
+								  "type": "object",
+								  "properties": {
+									"product": {
+									  "type": [
+										"string"
+									  ]
+									},
+									"amount": {
+									  "type": [
+										"number"
+									  ]
+									},
+									"price": {
+									  "type": [
+										"number"
+									  ]
+									}
+								  }
+								}
+							  }
+							}
+						  }
+					],
+					"global": {
+						"$schema": "http://json-schema.org/draft-07/schema#",
+						"type": "object",
+						"properties": {
+						  "context": {
+							"required": [
+							  "library"
+							],
+							"type": "object",
+							"properties": {
+							  "library": {"type": ["object"]}
+							}
+						  },
+						  "traits": {},
+						  "properties": {}
+						}
+					},
+					"identify": {
+						"$schema": "http://json-schema.org/draft-07/schema#",
+						"type": "object",
+						"properties": {
+						  "traits": {
+							"type": "object",
+							"properties": {
+							  "occupation": {
+								"type": [
+								  "string"
+								]
+							  },
+							  "age": {
+								"type": [
+								  "number"
+								]
+							  },
+							  "name": {
+								"type": [
+								  "string"
+								]
+							  }
+							},
+							"required": [
+							  "name"
+							]
+						  },
+						  "properties": {},
+						  "context": {}
+						}
+					},
+					"group": {
+						"$schema": "http://json-schema.org/draft-07/schema#",
+						"type": "object",
+						"properties": {
+						  "properties": {},
+						  "context": {},
+						  "traits": {
+							"properties": {
+							  "company": {
+								"type": [
+								  "object"
+								]
+							  }
+							},
+							"required": [
+							  "company"
+							],
+							"type": "object"
+						  }
+						}
+					}
+				},
+				"create_time": "2019-02-05T01:21:25Z",
+				"update_time": "2019-02-05T01:21:25Z"
+			}`)
+	})
+
+	actual, err := client.GetTrackingPlan(testTrackingPlan)
+	assert.NoError(t, err)
+
+	createTime, _ := time.Parse(time.RFC3339, "2019-02-05T01:21:25Z")
+	updatedTime, _ := time.Parse(time.RFC3339, "2019-02-05T01:21:25Z")
+	expected := TrackingPlan{
+		Name:        "workspaces/myworkspace/tracking-plans/rs_123",
+		DisplayName: "Kicks App",
+		Rules: Rules{
+			IdentifyTraits: []Rule{},
+			GroupTraits:    []Rule{},
+			Events: []Event{
+				Event{
+					Name:        "Order Completed",
+					Description: "Who bought what",
+					Version:     1,
+					Rules: Rule{
+						Schema: "http://json-schema.org/draft-07/schema#",
+						Type:   "object",
+						Properties: map[string]Rule{
+							"context": Rule{},
+							"traits":  Rule{},
+							"properties": Rule{
+								Required: []string{"product", "price", "amount"},
+								Type:     "object",
+								Properties: map[string]Rule{
+									"product": Rule{
+										Type: []interface{}{"string"},
+									},
+									"amount": Rule{
+										Type: []interface{}{"number"},
+									},
+									"price": Rule{
+										Type: []interface{}{"number"},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			Global: Rule{
+				Schema: "http://json-schema.org/draft-07/schema#",
+				Type:   "object",
+				Properties: map[string]Rule{
+					"context": Rule{
+						Required: []string{"library"},
+						Type:     "object",
+						Properties: map[string]Rule{
+							"library": Rule{
+								Type: []interface{}{"object"},
+							},
+						},
+					},
+					"traits":     Rule{},
+					"properties": Rule{},
+				},
+			},
+			Identify: Rule{
+				Schema: "http://json-schema.org/draft-07/schema#",
+				Type:   "object",
+				Properties: map[string]Rule{
+					"traits": Rule{
+						Type: "object",
+						Properties: map[string]Rule{
+							"occupation": Rule{
+								Type: []interface{}{"string"},
+							},
+							"age": Rule{
+								Type: []interface{}{"number"},
+							},
+							"name": Rule{
+								Type: []interface{}{"string"},
+							},
+						},
+						Required: []string{"name"},
+					},
+					"properties": Rule{},
+					"context":    Rule{},
+				},
+			},
+			Group: Rule{
+				Schema: "http://json-schema.org/draft-07/schema#",
+				Type:   "object",
+				Properties: map[string]Rule{
+					"properties": Rule{},
+					"context":    Rule{},
+					"traits": Rule{
+						Properties: map[string]Rule{
+							"company": Rule{
+								Type: []interface{}{"object"},
+							},
+						},
+						Required: []string{"company"},
+						Type:     "object",
+					},
+				},
+			},
+		},
+		CreateTime: createTime,
+		UpdateTime: updatedTime,
+	}
+	fmt.Printf("ACTUAL:: %+v\n", actual)
 	assert.Equal(t, expected, actual)
 }
