5b7acdbc65beca1ce8f311ceace8e31f10f3f4bd
rae89 <raespinoza109@gmail.com>
2019-09-06 17:04:17 -0700

    add GetTrackingPlan method

--- segment/tracking_plans.go
+++ segment/tracking_plans.go
@@ -23,3 +23,19 @@ func (c *Client) ListTrackingPlans() (TrackingPlans, error) {
 
 	return p, nil
 }
+
+// GetTrackingPlan returns information about a tracking plan
+func (c *Client) GetTrackingPlan(planName string) (TrackingPlan, error) {
+	var p TrackingPlan
+	data, err := c.doRequest(http.MethodGet,
+		fmt.Sprintf("%s/%s/%s/%s", WorkspacesEndpoint, c.workspace, TrackingPlanEndpoint, planName),
+		nil)
+	if err != nil {
+		return p, err
+	}
+	err = json.Unmarshal(data, &p)
+	if err != nil {
+		return p, errors.Wrap(err, "failed to unmarshal tracking plan response")
+	}
+	return p, nil
+}
