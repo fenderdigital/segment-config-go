e6107c13a1eca6c0b6b55b14393036ea0a014790
rae89 <raespinoza109@gmail.com>
2019-09-08 11:44:28 -0700

    add test for create tracking plan

--- segment/tracking_plans_test.go
+++ segment/tracking_plans_test.go
@@ -292,3 +292,234 @@ func TestTrackingPlans_GetTrackingPlan(t *testing.T) {
 	fmt.Printf("ACTUAL:: %+v\n", actual)
 	assert.Equal(t, expected, actual)
 }
+
+func TestTrackingPlans_CreateTrackingPlan(t *testing.T) {
+	setup()
+	defer teardown()
+
+	testDisplayName := "Kicks App"
+	testRules := Rules{
+		Events: []Event{
+			Event{
+				Name:        "Order Completed",
+				Description: "Who bought what",
+				Version:     1,
+				Rules: Rule{
+					Schema: "http://json-schema.org/draft-07/schema#",
+					Type:   "object",
+					Properties: map[string]Rule{
+						"context": Rule{},
+						"traits":  Rule{},
+						"properties": Rule{
+							Required: []string{"product", "price", "amount"},
+							Type:     "object",
+							Properties: map[string]Rule{
+								"product": Rule{
+									Type: []interface{}{"string"},
+								},
+								"amount": Rule{
+									Type: []interface{}{"number"},
+								},
+								"price": Rule{
+									Type: []interface{}{"number"},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		Global: Rule{
+			Schema: "http://json-schema.org/draft-07/schema#",
+			Type:   "object",
+			Properties: map[string]Rule{
+				"context": Rule{
+					Required: []string{"library"},
+					Type:     "object",
+					Properties: map[string]Rule{
+						"library": Rule{
+							Type: []interface{}{"object"},
+						},
+					},
+				},
+				"traits":     Rule{},
+				"properties": Rule{},
+			},
+		},
+		Identify: Rule{
+			Schema: "http://json-schema.org/draft-07/schema#",
+			Type:   "object",
+			Properties: map[string]Rule{
+				"traits": Rule{
+					Type: "object",
+					Properties: map[string]Rule{
+						"occupation": Rule{
+							Type: []interface{}{"string"},
+						},
+						"age": Rule{
+							Type: []interface{}{"number"},
+						},
+						"name": Rule{
+							Type: []interface{}{"string"},
+						},
+					},
+					Required: []string{"name"},
+				},
+				"properties": Rule{},
+				"context":    Rule{},
+			},
+		},
+		Group: Rule{
+			Schema: "http://json-schema.org/draft-07/schema#",
+			Type:   "object",
+			Properties: map[string]Rule{
+				"properties": Rule{},
+				"context":    Rule{},
+				"traits": Rule{
+					Properties: map[string]Rule{
+						"company": Rule{
+							Type: []interface{}{"object"},
+						},
+					},
+					Required: []string{"company"},
+					Type:     "object",
+				},
+			},
+		},
+	}
+
+	endpoint := fmt.Sprintf("/%s/%s/%s/%s/",
+		apiVersion, WorkspacesEndpoint, testWorkspace, TrackingPlanEndpoint)
+
+	mux.HandleFunc(endpoint, func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, `{
+			"tracking_plan": {
+			"display_name": "Kicks App",
+			"rules": {
+				"events": [
+					{
+						"name": "Order Completed",
+						"description": "Who bought what",
+						"version": 1,
+						"rules": {
+						  "$schema": "http://json-schema.org/draft-07/schema#",
+						  "type": "object",
+						  "properties": {
+							"context": {},
+							"traits": {},
+							"properties": {
+							  "required": [
+								"product",
+								"price",
+								"amount"
+							  ],
+							  "type": "object",
+							  "properties": {
+								"product": {
+								  "type": [
+									"string"
+								  ]
+								},
+								"amount": {
+								  "type": [
+									"number"
+								  ]
+								},
+								"price": {
+								  "type": [
+									"number"
+								  ]
+								}
+							  }
+							}
+						  }
+						}
+					  }
+				],
+				"global": {
+					"$schema": "http://json-schema.org/draft-07/schema#",
+					"type": "object",
+					"properties": {
+					  "context": {
+						"required": [
+						  "library"
+						],
+						"type": "object",
+						"properties": {
+						  "library": {"type": ["object"]}
+						}
+					  },
+					  "traits": {},
+					  "properties": {}
+					}
+				},
+				"identify": {
+					"$schema": "http://json-schema.org/draft-07/schema#",
+					"type": "object",
+					"properties": {
+					  "traits": {
+						"type": "object",
+						"properties": {
+						  "occupation": {
+							"type": [
+							  "string"
+							]
+						  },
+						  "age": {
+							"type": [
+							  "number"
+							]
+						  },
+						  "name": {
+							"type": [
+							  "string"
+							]
+						  }
+						},
+						"required": [
+						  "name"
+						]
+					  },
+					  "properties": {},
+					  "context": {}
+					}
+				},
+				"group": {
+					"$schema": "http://json-schema.org/draft-07/schema#",
+					"type": "object",
+					"properties": {
+					  "properties": {},
+					  "context": {},
+					  "traits": {
+						"properties": {
+						  "company": {
+							"type": [
+							  "object"
+							]
+						  }
+						},
+						"required": [
+						  "company"
+						],
+						"type": "object"
+					  }
+					}
+				}
+			}
+		  }
+		}`)
+	})
+
+	expected := trackingPlanCreateRequest{
+		TrackingPlan: TrackingPlan{
+			DisplayName: testDisplayName,
+			Rules:       testRules},
+	}
+
+	actual, err := client.CreateTrackingPlan(testDisplayName, testRules)
+
+	assert.NoError(t, err)
+
+	assert.Equal(t, expected, actual)
+
+}
