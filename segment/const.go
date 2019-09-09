897b6d803d5bf05b6969d65d7867d55348bbc2b3
rae89 <raespinoza109@gmail.com>
2019-09-09 14:25:50 -0700

    add constant

--- segment/const.go
+++ segment/const.go
@@ -7,6 +7,8 @@ const (
 	SourceEndpoint = "sources"
 	// DestinationEndpoint is the API endpoint for interacting with destinations
 	DestinationEndpoint = "destinations"
-	// TrackingPlanEndpoint is the API endpoint for the interacting with tracking plans
+	// TrackingPlanEndpoint is the API endpoint for interacting with tracking plans
 	TrackingPlanEndpoint = "tracking-plans"
+	// TrackingPlanSourceConnectionEndpoint is the API endpoint for the connecting a source to a tracking plan
+	TrackingPlanSourceConnectionEndpoint = "source-connections"
 )
