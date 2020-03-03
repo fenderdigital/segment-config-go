f3fcb5bacde7670944fe7798915dea0854ae2706
rae89 <raespinoza109@gmail.com>
2020-03-03 14:45:28 -0800

    make changes

--- segment/types.go
+++ segment/types.go
@@ -57,11 +57,11 @@ type TrackingPlans struct {
 
 // TrackingPlan defines the struct for the destination object
 type TrackingPlan struct {
-	Name        string      `json:"name,omitempty"`
-	DisplayName string      `json:"display_name,omitempty"`
-	Rules       interface{} `json:"rules,omitempty"`
-	CreateTime  *time.Time  `json:"create_time,omitempty"`
-	UpdateTime  *time.Time  `json:"update_time,omitempty"`
+	Name        string                 `json:"name,omitempty"`
+	DisplayName string                 `json:"display_name,omitempty"`
+	Rules       map[string]interface{} `json:"rules,omitempty"`
+	CreateTime  *time.Time             `json:"create_time,omitempty"`
+	UpdateTime  *time.Time             `json:"update_time,omitempty"`
 }
 
 // Rules contains the information about all the rules of a tracking plan
